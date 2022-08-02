a0881a9b2d2a5351b53276f6448d3902cdc9aefb
sergio.sereno@primaverabss.com <Sereno?GH?75>
2022-08-02 15:56:57 +0100

    Refactoring to support VS2022 and latter

---
+++ src/Primavera.Extensibility.Core/ExtensibilityProject1/ExtensibilityProject1/PriCustomTab1.cs
@@ -0,0 +1,21 @@
+using Primavera.Extensibility.Accounting.Editors
+using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomTab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+;
+
+namespace ExtensibilityProject1
+{
+    public partial class PriCustomTab1 : CustomTab<ConsolidacaoSaldos>
+    {
+        public PriCustomTab1()
+        {
+            InitializeComponent();
+        }
+    }
+}
