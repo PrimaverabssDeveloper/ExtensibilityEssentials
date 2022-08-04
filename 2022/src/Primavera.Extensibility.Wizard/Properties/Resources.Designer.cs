3402a512d398de20564619762bb2ef207de30842
sergio.sereno@primaverabss.com <Sereno?GH?75>
2022-08-04 16:00:35 +0100

    new icon

--- 2022/src/Primavera.Extensibility.Wizard/Properties/Resources.Designer.cs
+++ 2022/src/Primavera.Extensibility.Wizard/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Primavera.Extensibility.Wizard.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap V101 {
+            get {
+                object obj = ResourceManager.GetObject("V101", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
